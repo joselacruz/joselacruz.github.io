0b33cb1431612a3551df41d5e721b4d8e2de2de7
luixlacrux <luixlacrux18@gmail.com>
2017-05-09 14:00:57 -0400

    [add] send contact form via ajax

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,4 +1,7 @@
 var smoothScroll = require('smooth-scroll')
+var axios = require('axios')
+var notify = require('izitoast')
+
 function ready(fn) {
   if (document.readyState != 'loading'){
     fn();
@@ -12,4 +15,48 @@ ready(function () {
     easing: 'easeInOutCubic',
     speed: 600
   })
+
+  /* Contact Form */
+  var contactForm = document.getElementById('contact-form')
+  var button = document.getElementById('btn-send')
+
+  var baseURL = 'https://formspree.io/luislacruz.dev@gmail.com'
+
+  contactForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    loadingButton(true)
+
+    axios({
+      method: 'POST',
+      url: baseURL,
+      data: new FormData(contactForm),
+      responseType: 'json',
+    })
+    .then(function (response) {
+      loadingButton(false)
+      notify.success({
+        title: 'OK',
+        message: 'Email enviado exitosamente.',
+      });
+      // clean form
+      event.target.reset()
+    })
+    .catch(function (error) {
+      loadingButton(false)
+      notify.error({
+        title: 'Oops',
+        message: 'Ocurrio un error en el servidor, intente otra vez',
+      })
+    })
+  })
+
+  function loadingButton (boolean) {
+    if (boolean) {
+      button.innerHTML = 'Enviando...'
+      button.disabled = true
+    } else {
+      button.innerHTML = 'Enviar'
+      button.disabled = false
+    }
+  }
 })
